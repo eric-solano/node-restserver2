4b39ea418828aeb03c12923060edd1c136d4825d
Eric Solano <eric.solano.levano@gmail.com>
2020-04-25 14:39:27 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB = 'mongodb+srv://eric_solano:KeHQVUO9yCLwtjRN@cluster0-e38m4.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;//'mongodb+srv://eric_solano:KeHQVUO9yCLwtjRN@cluster0-e38m4.mongodb.net/cafe'
 }
 
 process.env.URLDB = urlDB
