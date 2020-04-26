77b6d02e87c76cc572354bbd4075cd0f13d1cf84
Eric Solano <eric.solano.levano@gmail.com>
2020-04-26 00:01:14 -0500

    Primer commit

---
+++ server/routes/usuario98.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const Usuario = require('../models/usuario98');
+const app = express();
+
+app.get('/usuario', function (req, res) {
+    res.json('get Usuario LOCAL!!!')
+  });
+  
+app.post('/usuario', function (req, res) {
+  let body = req.body;
+  let usuario = new Usuario({
+    nombre: body.nombre,
+    email: body.email,
+    password: bcrypt.hashSync(body.password, 10),
+    //img: body.img,
+    role: body.role
+  });
+
+  usuario.save( (err,usuarioDB) => {
+    if(err){
+      return res.status(400).json({
+        ok: false,
+        err
+      })
+    }
+    //usuarioDB.password = null;
+    res.json({
+      ok:true,
+      usuario: usuarioDB
+    })
+
+  })
+  
+});
+
+//Sirve para actualizar un registro
+app.put('/usuario/:id', function (req, res) {
+  let id = req.params.id;
+  let body = req.body;
+
+  //findByIdAndUpdate(Id a actualizar, objeto a actualizar, devuelve el objeto actualizado , callback
+  Usuario.findByIdAndUpdate(id, body, {new:true}, (err,usuarioDB) => {
+    if(err){
+      return res.status(400).json({
+        ok: false,
+        err
+      })
+    }
+    
+    res.json({
+      ok: true,
+      usuario: usuarioDB
+    });
+  });
+  
+});
+
+app.delete('/usuario', function (req, res) {
+  res.json('delete Usuario')
+});
+
+  module.exports = app;
---
+++ server/server85.js
@@ -0,0 +1,6 @@
+/*
+heroku create -h
+git add . / git commit -am "mensaje"
+git commit -m
+git push heroku master
+*/
